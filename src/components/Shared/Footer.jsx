1cf3e9c15dba9d919882b8dcb9704c5bb279cbcf
Mh-Monzil <monzil246@gmail.com>
2024-07-04 02:42:13 +0600

    add: footer added

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,9 +1,38 @@
-
+import Logo from "./Logo";
+import { FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <div>
-            Footer
+            {/* col-1  */}
+            <div>
+                <Logo />
+                <p>Our family-centered approach to healthcare ensure that each member of your family receives personalized attention</p>
+                <div>
+                <FaLinkedin />
+                <FaYoutube />
+                <FaTwitter />
+                <FaSquareInstagram />
+                <FaFacebookSquare />
+                </div>
+            </div>
+            
+            {/* col-2  */}
+            <div>
+                <p>Quick Links</p>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About Us</Link>
+            </div>
+
+            {/* col-3  */}
+            <div></div>
+
+            {/* col-4  */}
+            <div></div>
+
         </div>
     );
 };
