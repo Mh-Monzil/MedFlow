6f392895b59d9257528ed0adb7667715534b5383
Mh-Monzil <monzil246@gmail.com>
2024-07-03 23:24:51 +0600

    add: icon added

--- src/components/Home/AppointmentBox.jsx
+++ src/components/Home/AppointmentBox.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
-
+import { RiFirstAidKitFill } from "react-icons/ri";
+import { MdOutlineEventAvailable } from "react-icons/md";
 
 const AppointmentBox = () => {
     return (
-        <div>
-            <p>icon</p>
+        <div className="bg-primaryDark">
+            <div className="max-w-7xl mx-auto px-4 md:px-6 flex flex-col md:flex-row items-center justify-evenly">
+            <RiFirstAidKitFill />
             <div>
                 <h2>Open For Appointments</h2>
                 <p>We are delighted to announce that out doors are open, and we are now accepting appointments to serve you better.</p>
             </div>
             <Link>
                 <span>Make Appointment</span>
-                <span>icon</span>
+                <MdOutlineEventAvailable />
             </Link>
+            </div>
         </div>
     );
 };
