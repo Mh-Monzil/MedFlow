9ea9e3d125f676d5cde5d6bbe524cce4b5499db5
Mh-Monzil <monzil246@gmail.com>
2024-07-09 01:04:44 +0600

    footer all content added

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,40 +1,69 @@
 import Logo from "./Logo";
-import { FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaLinkedin, FaPhoneAlt, FaTwitter, FaYoutube } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { MdLocationPin, MdOutlineAccessTimeFilled } from "react-icons/md";
+import { RiMailSendFill } from "react-icons/ri";
 
 const Footer = () => {
-    return (
+  return (
+    <div className="">
+      {/* col-1  */}
+      <div>
+        <Logo />
+        <p>
+          Our family-centered approach to healthcare ensure that each member of
+          your family receives personalized attention
+        </p>
         <div>
-            {/* col-1  */}
-            <div>
-                <Logo />
-                <p>Our family-centered approach to healthcare ensure that each member of your family receives personalized attention</p>
-                <div>
-                <FaLinkedin />
-                <FaYoutube />
-                <FaTwitter />
-                <FaSquareInstagram />
-                <FaFacebookSquare />
-                </div>
-            </div>
-            
-            {/* col-2  */}
-            <div>
-                <p>Quick Links</p>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About Us</Link>
-            </div>
-
-            {/* col-3  */}
-            <div></div>
-
-            {/* col-4  */}
-            <div></div>
-
+          <FaLinkedin />
+          <FaYoutube />
+          <FaTwitter />
+          <FaSquareInstagram />
+          <FaFacebookSquare />
         </div>
-    );
+      </div>
+
+      {/* col-2  */}
+      <div>
+        <p>Quick Links</p>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+      </div>
+
+      {/* col-3  */}
+      <div>
+        <p>Contact Details</p>
+        <p>
+          <MdLocationPin />
+          Dhaka, Bangladesh
+        </p>
+        <p>
+          <RiMailSendFill />
+          medflow@gmail.com
+        </p>
+        <p>
+          <FaPhoneAlt />
+          1234567890
+        </p>
+        <p>
+          <MdOutlineAccessTimeFilled />
+          8 AM - 10 PM, Monday - Saturday
+        </p>
+      </div>
+
+      {/* col-4  */}
+      <div>
+        <p>Subscribe To Our Newsletter</p>
+        <p>Stay informed and never miss out on the latest news, health tips.</p>
+        <div>
+            <input type="email" />
+            <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
